368c45595adfc6e3e40e9b1fe66a20f4deb4ad81
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-12-29 12:41:48 +0100

    Fix: restructure model comments

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const formidableMiddleware = require('express-formidable');
 const reservaRoutes = require('./routes/reserva.routes');
 const viviendaRoutes = require('./routes/vivienda.routes');
 const imagesRoutes = require('./routes/images.routes');
-const comentarioRoutes = require('./routes/comentario.routes');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
@@ -16,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/reservas', reservaRoutes);
 app.use('/viviendas', viviendaRoutes);
-app.use('/comentarios', comentarioRoutes);
 app.use('/images', [formidableMiddleware()], imagesRoutes);
 
 
