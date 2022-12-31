3f24f5ee71d3519b1732e50c0980e0043cdb998c
AngeloGC <0619974249@uma.es>
2022-12-31 02:16:05 +0100

    Finish auth

--- src/routes/images.routes.js
+++ src/routes/images.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const cloudinary = require( 'cloudinary' ).v2;
+const { validateAccessToken } = require("../middleware/auth0.js");
 cloudinary.config( process.env.CLOUDINARY_URL );
 
 
 //subir imagen a cloudinary
-router.post('/uploadImage', async (req, res) => {
+router.post('/uploadImage', validateAccessToken, async (req, res) => {
     const { url } = await cloudinary.uploader.upload( req.files.image.path, { folder: 'IWEB2223' });
  
     return res.json({url});
--- src/routes/reserva.routes.js
+++ src/routes/reserva.routes.js
@@ -4,28 +4,28 @@ const router = express.Router();
 const { validateAccessToken } = require("../middleware/auth0.js");
 
 // Create a new reserva
-router.post('/', reservas.create);
+router.post('/', validateAccessToken, reservas.create);
 
 //Retrieve all reservas
-router.get('/', reservas.findAll);
+router.get('/', validateAccessToken, reservas.findAll);
 
 // Retrieve a single reserva with id
-router.get('/:id', reservas.findOne);
+router.get('/:id', validateAccessToken, reservas.findOne);
 
 //Retrieve all reservas with antique
-router.get('/antiguedad/:fecha', reservas.findByAntique);
+router.get('/antiguedad/:fecha', validateAccessToken, reservas.findByAntique);
 
 // Update a reserva with id
-router.put('/:id', reservas.update);
+router.put('/:id', validateAccessToken, reservas.update);
 
 // Delete a reserva with id
-router.delete('/:id', reservas.delete);
+router.delete('/:id', validateAccessToken, reservas.delete);
 
 // Retrieve all reservas after a date. Angel FC
-router.get('/estancia/:fechaInicio', reservas.findByFutureDate);
+router.get('/estancia/:fechaInicio', validateAccessToken, reservas.findByFutureDate);
 
 // Retrieve vivienda from a reserva
-router.get('/:id/vivienda', reservas.findVivienda);
+router.get('/:id/vivienda', validateAccessToken, reservas.findVivienda);
 
 // Retrieve all reservas of a person
 router.get('/usuario/:id', validateAccessToken, reservas.findByPerson);
